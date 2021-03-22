8e4ee7d3bdad72f0a26641e1c1cc8a23d95ef60e
Matiya DERUELLE <mderuell@e3r1p4.42.fr>
2021-03-22 17:29:34 +0100

    first

---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mderuell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 17:14:58 by mderuell          #+#    #+#             */
+/*   Updated: 2021/03/22 17:27:13 by mderuell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	int a;
+	int b;
+
+	i = 0;
+	a = 0;
+	b = 0;
+	while (i <= n)
+	{
+		while (s1[a] && i == n)
+			a++;
+		while (s2[b] && i == n)
+			b++;
+		i++;
+	}
+	return (a - b);
+
+}
+#include <stdio.h>
+int main()
+{
+	char s1[] = "123456";
+	char s2[] = "1234";
+
+	printf("%d\n",ft_strncmp(s1, s2,1));
+	return 0;
+}
